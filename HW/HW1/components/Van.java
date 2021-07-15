0865c5559909960ab3e9bcccb11a223f7440d38e
danieel47896321 <74861262+danieel47896321@users.noreply.github.com>
2021-07-15 09:04:11 +0300

    Update Van.java

--- HW/HW1/components/Van.java
+++ HW/HW1/components/Van.java
@@ -1,21 +1,27 @@
 package components;
+
 public class Van extends Truck{
+	
 	public Van() {
 		super();
-		System.out.println("Creating " + toString());
+		System.out.println(this);
 	}
-	public Van(String licensePlate, String truckModel) {
-		super(licensePlate, truckModel);
-		System.out.println("Creating " + toString());
+	
+	
+	public Van(String licensePlate,String truckModel) {
+		super(licensePlate,truckModel);
+		System.out.println(this);
 	}
-	public String toString() { return "Van [" + super.toString() + "]"; }
-	public boolean equals(Object object) {
-		if(object == null || this.getClass() != object.getClass() || !super.equals(object)) 
-			return false; 
-		Van other = (Van)object;
-		return this.equals(other);
+	
+
+	@Override
+	public String toString() {
+		return "Van ["+ super.toString() + "]";
 	}
-	public void deliverPackage(Package p) {	
+	
+	
+	@Override
+	public void deliverPackage(Package p) {
 		addPackage(p);
 		setAvailable(false);
 		int time=p.getDestinationAddress().getStreet()%10+1;
@@ -23,22 +29,25 @@ public class Van extends Truck{
 		p.addRecords(Status.DISTRIBUTION, this);
 		System.out.println(getName() + " is delivering " + p.getName() + ", time left: "+ getTimeLeft()  );
 	}
-
-	public void work() {		
+	
+	
+	@Override
+	public  void work() {
 		if (!isAvailable()) {
 			setTimeLeft(getTimeLeft()-1);
-			if (this.getTimeLeft() == 0){
+			if (this.getTimeLeft()==0){
 				for (Package p : this.getPackages()) {
-					if (p.getStatus() == Status.COLLECTION) {
+					if (p.getStatus()==Status.COLLECTION) {
 						p.addRecords(Status.BRANCH_STORAGE,p.getSenderBranch());
-						System.out.println(getName() + " has collected " + p.getName() + " and arrived back to " + p.getSenderBranch().getName());
+						System.out.println(getName() + " has collected " +p.getName()+" and arrived back to " + p.getSenderBranch().getName());
 					}
 					else {
 						p.addRecords(Status.DELIVERED, null);
 						p.getDestBranch().removePackage(p);
-						System.out.println(getName() + " has delivered " + p.getName() + " to the destination");
-						if (p instanceof SmallPackage && ((SmallPackage)p).isAcknowledge()) 
-							System.out.println("Acknowledge sent for " + p.getName());
+						System.out.println(getName() + " has delivered "+p.getName() + " to the destination");
+						if (p instanceof SmallPackage && ((SmallPackage)p).isAcknowledge()) {
+							System.out.println("Acknowledge sent for "+p.getName());
+						}
 					}
 				}
 				this.getPackages().removeAll(getPackages());
