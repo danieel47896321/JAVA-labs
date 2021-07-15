3dff91be599241dfae0c255bf7b6d9a22be87c46
danieel47896321 <74861262+danieel47896321@users.noreply.github.com>
2021-07-15 09:01:25 +0300

    Update Hub.java

--- HW/HW1/components/Hub.java
+++ HW/HW1/components/Hub.java
@@ -1,28 +1,28 @@
 package components;
+
 import java.util.ArrayList;
 import java.util.Random;
-/**
- * @author Daniel Elmaliach
- * @ID 206068629
- * The Class Hub.
- */
-public class Hub extends Branch implements Node{
-	private ArrayList<Branch> branches = new ArrayList<Branch>();
+
+public class Hub extends Branch{
+	
+	private ArrayList<Branch> branches=new ArrayList<Branch>();
 	private int currentIndex=0;
-	public Hub() { super("HUB"); }
-	public ArrayList<Branch> getBranches() { return branches;}
-	public void add_branch(Branch branch) { branches.add(branch); }
-	public void localWork() {
-		for (Truck t : listTrucks) {
-			if (t.isAvailable()){
-				if(t instanceof NonStandardTruck) 
-					shipNonStandard((NonStandardTruck)t);
-				else {
-					sendTruck((StandardTruck)t);
-				}
-			}	
-		}	
+	
+	public Hub() {
+		super("HUB");
 	}
+	
+
+	public ArrayList<Branch> getBranches() {
+		return branches;
+	}
+
+	
+	public void add_branch(Branch branch) {
+		branches.add(branch);
+	}
+
+	
 	public void shipNonStandard(NonStandardTruck t) {
 		for (Package p: packages) {
 			if (p instanceof NonStandardPackage) {
@@ -36,6 +36,21 @@ public class Hub extends Branch implements Node{
 			}
 		}	
 	}
+	
+	
+	@Override
+	public void localWork() {
+		for (Truck t : listTrucks) {
+			if (t.isAvailable()){
+				if(t instanceof NonStandardTruck) 
+					shipNonStandard((NonStandardTruck)t);
+				else {
+					sendTruck((StandardTruck)t);
+				}
+			}	
+		}	
+	}
+
 	public void sendTruck(StandardTruck t) {
 		t.setAvailable(false);
 		t.setDestination(branches.get(currentIndex));
@@ -44,4 +59,6 @@ public class Hub extends Branch implements Node{
 		System.out.println(t.getName() + " is on it's way to " + t.getDestination().getName() + ", time to arrive: "+t.getTimeLeft());	
 		currentIndex=(currentIndex+1)%branches.size();
 	}
+	
+
 }
